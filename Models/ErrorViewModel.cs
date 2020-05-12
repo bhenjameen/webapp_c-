b1bebf63ef7723f5c37a0dcf960fba709d52da5d
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-05-12 13:30:57 +0100

    update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqrRootWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
