16ab5bd127d84ad933034a38a6ec66c11ae0a3b6
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-05-12 15:14:38 +0100

    update

--- Controllers/CalcController.cs
+++ Controllers/CalcController.cs
@@ -25,7 +25,7 @@ namespace SqrRootWebApp.Controllers {
             } else if (secondRoot > firstRoot) {
                 result = ("The number " + numeroTwo + " with Square root " + secondRoot + " has a higher square root than the number " + numeroOne + " with square root " + firstRoot);
             } else if (firstRoot == secondRoot) {
-                result = ("The number " + numeroTwo + " with Square root " + secondRoot + " has the same square root as the number " + numeroOne + " with square root " + firstRoot);
+                result = ("The number " + numeroTwo + " with Square root " + secondRoot + " has the same square root as the number " + numeroOne + " with square root " + firstRoot + ". \n\nPlease enter another set of values. Try inputting different numbers.");
             }
             //} else if (int.TryParse (secondNumber, out numeroTwo)) {
             //    int firstRoot = Convert.ToInt32 (Math.Sqrt (numeroOne));
